749983c379f977a4ce4b7c663110fe6826fd1154
coriagogo <pkcparis@gmail.com>
2019-04-29 16:44:36 -0400

    searchbar complete

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -9,16 +9,15 @@ const SearchBar = () => {
             <div className="logo-imgs">
                 <i className="fab fa-instagram"></i> 
                 <span className="divider"> | </span>
-                <img className="iglogo" src={logo}/>
-                
+                <img className="ig-logo" src={logo}/>                
             </div>
             <div>
                 <input type="text" placeholder="&#x1F50D; Search..." />
             </div>
             <div className="social-icons">
-                <i class="far fa-compass"></i>
-                <i class="far fa-heart"></i>
-                <i class="far fa-user-circle"></i>
+                <i className="far fa-compass"></i>
+                <i className="far fa-heart"></i>
+                <i className="far fa-user-circle"></i>
             </div>
             
         </div>
