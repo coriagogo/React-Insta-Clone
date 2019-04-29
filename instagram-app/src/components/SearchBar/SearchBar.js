8ed4780ca57c7a9a1b4409572fe66b68ad699edd
coriagogo <pkcparis@gmail.com>
2019-04-29 16:22:47 -0400

    search header created but margins are off

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import logo from '../../assets/iglogo.png';
-import camera from '../../assets/camera-logo.svg';
+
+import './searchbar.css';
 
 const SearchBar = () => {
     return (
-        <div classname="search-bar-container">
+        <div className="search-container">
             <div className="logo-imgs">
-                <img src={logo}/>
-                <img src={camera}/>
+                <i className="fab fa-instagram"></i> 
+                <span className="divider"> | </span>
+                <img className="iglogo" src={logo}/>
+                
             </div>
             <div>
                 <input type="text" placeholder="Search..." />
             </div>
             <div className="social-icons">
-                
+                <i class="far fa-compass"></i>
+                <i class="far fa-heart"></i>
+                <i class="far fa-user-circle"></i>
             </div>
             
         </div>
